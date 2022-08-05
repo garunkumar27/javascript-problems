5bb3ee1f919585083e173aca43caaed28bda1e12
arun kumar <garunk.27@gmail.com>
2022-08-05 16:52:10 +0530

    Javascript Problems.

--- Addition/script.js
+++ Addition/script.js
@@ -1,5 +1,12 @@
 // Addition
 let ad,add,total;// variable declaration
-ad=10;add=50;// variable definition
+ad=parseFloat(prompt('Enter Number'));// To get Input from user
+add=parseFloat(prompt('Enter Number'));// To get Input from user
 total=ad+add;// expression
 console.log(total);// printing statement
+
+//Directly applying Value
+let a,b,sum;
+a=20;b=30;// Variable definition
+sum=a+b;
+console.log(sum);
--- Multiplication/script.js
+++ Multiplication/script.js
@@ -1,5 +1,12 @@
-// To Find Multiplication
+//Multiplication
 let mul,mult,answer;// variable declaration
 mul=10;mult=10;// variable definition
 answer=mul*mult;// expression
 console.log(answer);// printing statement
+
+// Input From User
+let a,b,result;
+a=parseFloat(prompt('Enter Number'));//input from user
+b=parseFloat(prompt('Enter Number'));//input from user
+result=a*b;
+console.log(result);
