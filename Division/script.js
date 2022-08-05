44a9b787d537def16753e36ac628dec4cc6738fd
arun kumar <garunk.27@gmail.com>
2022-08-05 16:54:18 +0530

    Javascript Problems.

--- Division/script.js
+++ Division/script.js
@@ -3,3 +3,10 @@ let divi,div,answer;// variable declaration
 divi=100;div=5;// variable definition
 answer=divi/div;// expression
 console.log(answer);// printing statement
+
+//Getting Input from User
+let a,b,total;
+a=parseFloat(prompt('Enter Number'));//input from user
+b=parseFloat(prompt('Enter Number'));//input from user
+total=a/b;
+console.log(total);
