d827bdb48e4bc17c0e85aabc7ddf2becab873324
arun kumar <garunk.27@gmail.com>
2022-08-05 16:56:37 +0530

    Javascript Problems.

--- SI/script.js
+++ SI/script.js
@@ -1,4 +1,4 @@
-// To Find Simple Intrest_Pnr/100
+// Simple Intrest_Pnr/100
 let number,SI,P,n,r;// variable declaration
 number=100;// variable definition
 P=parseInt(prompt('Give P value'));//To get Input from user
@@ -6,3 +6,8 @@ n=parseInt(prompt('Give n value'));//To get Input from user
 r=parseInt(prompt('Give r value'));//To get Input from user
 SI=P*n*r/number;// expression
 console.log(SI);// printing statement
+
+let num,p,N,R,si;
+num=100;p=10000,N=12,R=2;
+si=p*N*R/num;
+console.log(si);
