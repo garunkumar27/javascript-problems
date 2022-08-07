afb6e8a4794804fe36bc34191130dbf30432ee33
arun kumar <garunk.27@gmail.com>
2022-08-07 21:58:03 +0530

    Javascript Problems.

--- Total_and_average/script.js
+++ Total_and_average/script.js
@@ -1,14 +1,15 @@
-let n,a=[],s,sum;
+let n,a=[],s,sum;// Variable Declaration
 s=0;
-n=parseInt(prompt('Number of terms'));
+n=parseInt(prompt('Number of terms'));// Input from User Number of terms 
 for(i=0;i<n;i++){
-    a[i]=parseFloat(prompt('Enter the Number'));
+    a[i]=parseFloat(prompt('Enter the Number'));// Storing values in array
 }
 for(i=0;i<n;i++){
     sum=a[i]+s;
     s=sum;
 }
-console.log(sum);
+console.log(sum);// Printing Statement
 let avg;
 avg=sum/n;
-console.log(avg);
+console.log(avg);// Printing Statement
+
