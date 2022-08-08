9bacd907f7a4a1bb9ce8e877473119b64068c1fb
arun kumar <garunk.27@gmail.com>
2022-08-08 21:38:54 +0530

    Javascript Problems.

---
+++ Greater_than_10/script.js
@@ -0,0 +1,8 @@
+// Finding whether the given number is greater than 10 or not
+let number=(parseInt(prompt("Enter the number")));
+if(number>10){
+    console.log('Given Number is greater than 10');
+}
+else{
+    console.log('Given number is lesser than 10');
+}
