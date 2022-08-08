aff4542af71284fff8ea43736ce5b459f2858d9d
arun kumar <garunk.27@gmail.com>
2022-08-08 17:28:01 +0530

    Javascript Problems.

---
+++ Positive_or_not/script.js
@@ -0,0 +1,15 @@
+let number=(parseInt(prompt("Enter the Value")));
+if(number==0){//(== to comparison)
+    console.log('Zero is not a Positive or Negative number');
+
+}
+else{
+    if (number>0){
+        console.log('Number is Positive');
+}
+else{
+    console.log('Number is Negative');
+}
+}
+// In the starting the number is checked whether it is equal to zero or not
+// if not then it moves to 2nd condition to check whether the number is positive or negative
