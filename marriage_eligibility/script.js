af6115c3ec917949437b2c5a1af60f8955123580
arun kumar <garunk.27@gmail.com>
2022-08-09 16:57:39 +0530

    Javascript Problems.

---
+++ marriage_eligibility/script.js
@@ -0,0 +1,9 @@
+let age=parseInt(prompt("Enter the Age"));
+if(age>=21){
+    console.log("Eligible");
+}
+else{
+    console.log("Not Eligible");
+}
+
+// If the people's age is 21 or above they are eligible,if not they are not eligible  
