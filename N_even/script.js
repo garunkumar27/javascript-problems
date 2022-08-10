a8cf729aa3e2ba1ad69704c28d8701d6022c4ba7
arun kumar <garunk.27@gmail.com>
2022-08-10 16:54:03 +0530

    Javascript Problems.

---
+++ N_even/script.js
@@ -0,0 +1,7 @@
+let n=parseInt(prompt("Enter number"));
+for(i=1;i<=n;i++){
+    if(i%2==0){
+        console.log(i);
+    }
+}
+// TO Find whether the remainder is equal to zero or not by using Modulus operator. 
