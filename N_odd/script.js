ba54cf4d3b57ebf5cc5c2f4a08c9536394b344cf
arun kumar <garunk.27@gmail.com>
2022-08-12 09:57:53 +0530

    Javascript Problems.

---
+++ N_odd/script.js
@@ -0,0 +1,8 @@
+let n=parseInt(prompt("Enter value"));
+for(i=0;i<n;i++){
+    if(i%2==1){
+        console.log(i);
+    }
+}
+
+// TO Find whether the remainder is equal to one or not by using Modulus operator.
