19ee49bd7c8c899404aad2f9af450015b7b53bfd
arun kumar <garunk.27@gmail.com>
2022-08-17 16:36:39 +0530

    Javascript Problems.

--- Prime_number_or_not/script.js
+++ Prime_number_or_not/script.js
@@ -1,7 +1,7 @@
 // Prime number
 // The number that is divisible by 1 and divisible by the same number 
 let n=parseInt(prompt("Enter value"));
-let s=1;
+let s=0;
 for(i=2;i<n;i++){
     if(n%i==0){
         s=1;
@@ -9,11 +9,9 @@ for(i=2;i<n;i++){
     }
 }
     if(s==1){
-        console.log("Not Prime Number");
+        console.log("Not a Prime Number");
     }
     else{
         console.log("Prime Number");
     }
-
-    // 
-    // 
+ 
