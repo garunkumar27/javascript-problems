07c1cc03f7c2e447bc74c39b2ad13e4205664038
arun kumar <garunk.27@gmail.com>
2022-08-27 09:56:57 +0530

    All Codings.

--- z-practice/script.js
+++ z-practice/script.js
@@ -252,7 +252,7 @@
 //     }
 // }
 
-//"SET  FULL YEAR"
+// "SET  FULL YEAR"
 // const d =new Date();
 // (d.setFullYear(2020));
 // console.log(d);
@@ -382,6 +382,12 @@
 // }
 
 // Review on Condition
+// Question
+// Ask user to enter age, sex (M or F), marital status (Y or N) and then using following rules print their place of service. if employee is female, then she will work only in urban areas. 
+// if employee is a male and age is in between 20 to 40 then he may work in anywhere 
+// if employee is male and age is in between 40 to 60 then he will work in urban areas only.
+// And any other input of age should print "ERROR".
+
 // let age=parseInt(prompt("Enter the AGE"));
 // let gen=prompt("Enter male or female");
 // let mrg=prompt("Married Or Unmarried");
