7182528d33e4b1b4e78c48e554342026d42653f0
arun kumar <garunk.27@gmail.com>
2022-09-14 12:49:10 +0530

    All codings No:1.

--- z-practice/script.js
+++ z-practice/script.js
@@ -406,3 +406,378 @@
 // 	}
 // }  
 
+// DO WHILE
+//let i=0;
+//do{
+	//b=i;
+	//i++;
+	//console.log(b); 
+		
+	
+//}
+//while(i<=10);
+
+//let a=parseInt(prompt("Give The Number"));
+//for(i=0;i<=a;i++){
+//	console.log(a-i);
+//}
+//let a=parseInt(prompt("Give The Number"));
+//let i=0;
+//while(i<a){
+//	i++;
+//	console.log(a-i);
+//}
+
+//let a=parseInt(prompt("Give The Number"));
+//let i=0;
+//do{
+//	i++;
+//	console.log(a-i);
+//}
+//while(i<a);
+
+// let a={
+// 	name:"Arun",
+// 	Age:"22",
+// }
+// for(i in a){
+// 	console.log(i);
+// }
+// let b=[1,3,4,5,7,99,00,"Arun"];
+// for(i in b){
+// 	console.log(i);
+// }
+
+//ForEach Method 
+// let c=[1,3,4,5,7,99,00,"Arun"];
+// c.forEach((i)=>{
+// 	console.log(i)
+// })
+
+//Tables of the Number
+//let a= parseInt(prompt("Enter the table Value")),
+//b= parseInt(prompt("Enter the table number"));
+//for(i=1;i<=b;i++){
+//c=a*i;
+//console.log(i,'*',a,'=',c);
+//}
+
+//Cube of a Number upto Given Integer
+// let a= parseInt(prompt("Enter the table Value"));
+// let i=1;
+// while(i<=a){
+// 	b=i**3;
+// 	console.log(b);
+// 	i++;
+// }
+
+// TASK(01/09/22);
+// Write a Program that outputs all possibility to put +- or nothing between a numbers
+// (1,2,3,4,5,6,7,8,9- in these order),such that the result is 100 .
+// let a=[1,28,32,4,5,6,7,8,9],s=0,b;
+// for(i=0;i<a.length;i++){
+// 	b=a[i]+s;
+// 	s=b;
+	
+// }
+// console.log(s);
+
+//With two string write a program which string has more vowels:
+//let a=prompt("Enter the String"),
+//b=prompt("Enter the String"),
+//ab=0,ba=0,c=["a","e","i","o","u","A","E","I","O","U"];
+//for(i=0;i<a.length;i++){
+// 	for(j=0;j<c.length;j++){
+// 		if(a[i]==c[j]){
+// 			ab++;
+// 		}
+// 	}
+// }
+// console.log(ab);
+
+// for(i=0;i<b.length;i++){
+// 	for(j=0;j<c.length;j++){
+// 		if(b[i]==c[j]){
+// 			ba++;
+// 		}
+// 	}
+// }
+// console.log(ba);
+// if(ab==ba){
+// 	console.log("Both has same number of vowels")
+// }
+// else if(ab>ba){
+// 	console.log(a,"Has More Vowels")
+// }
+// else{
+// 	console.log(b,"Has More Vowels")
+// }
+
+// let a=prompt("give a first string"),
+// b=prompt("give a second string"),
+// res_a,res_b,
+// c=["a","e","i","o","u","A","E","I","O","U"];
+// function aclick(ax,cx){
+//     ab=0,bc=0;
+//     for(i=0;i<a.length;i++){
+//         for(j=0; j<c.length; j++){
+//             if(ax[i]==cx[j]){
+//                 ab++;
+                
+//             }
+        
+//         }
+//     return ab;
+// }
+// console.log(ab);
+// }
+// res_a=aclick(a,c);
+// res_b=aclick(b,c);
+// console.log(res_a);
+// console.log(res_b);
+// if(res_a>res_b){
+//     console.log("a has a more vowels")
+// }
+// else{
+// 	console.log("b has more vowels")
+// }		
+	
+
+
+//function cal(a,b){
+	//ans=a+b;
+	//return ans;
+//}
+//let e=cal(5,25);
+//console.log(e);
+
+//function calcu(x,y){
+//if(x>y){
+//		console.log("x is greater");
+//	}
+//	else
+//		console.log("Y is Greater");
+//}
+//let a=parseInt(prompt("Enter the value")),b=parseInt(prompt("Enter the value"));
+//calcu(a,b);//function call
+
+//let a=(prompt("Enter the text")),
+// let a="durai"
+
+// c=a.replace(durai,diamond);
+//c=a.slice(-5,-3);
+//console.log(c);
+
+
+// let a="Arun Kumar Arun Kumar Arun Kumar ";
+// console.log(a.match(/run/));
+
+// Reverse of N numbers
+// let n=20;
+// for(i=n;i>=0;i--){
+// 	console.log(i);
+// }
+
+// for(i=0;i<=5;i++){
+//for(i=5;i>=0;i--){
+//	for(j=0;j<=i;j++){
+//		document.write("*");
+//	}	
+//	document.write("<br>");	
+//}	
+//}
+
+
+//Math Function for Random Colors using hexa
+// function random(){ 
+
+// let color=0xFFFFFF;
+// let random_num = Math.random() * color;
+// random_num = Math.floor(random_num);
+// random_num = random_num.toString(16);
+// let rand_color=random_num.padStart(6,0);
+// return `#${rand_color.toUpperCase()}`
+// } 
+// console.log(random());
+ 
+//Math Function for Random Colors using rgb
+// function randomcolor(){
+// 	let a=Math.trunc(Math.random()*256),
+// 	b=Math.trunc(Math.random()*256),
+// 	c=Math.trunc(Math.random()*256),colors;
+// 	colors="rgb("+a+","+b+","+c+")";
+// 	document.body.style.background=colors;
+// 	console.log(colors);
+// }
+// randomcolor();
+
+
+
+// let n=parseInt(prompt("enter the number")),remain,a=[],i=0,
+// arr=["zero","one","two","three","four","five","six","seven","eight","nine"];
+// while (n > 0) {
+//     remain = n % 10;
+//     n = n / 10;
+// 	n = parseInt(n);
+// 	i=remain;
+// 		console.log(arr[i]);
+// 	}
+
+
+
+// function small_large(grt){
+//     t=-1;
+//     grt=grt.sort(function(a,b){return a-b})
+//     if(grt==[]){
+//     console.log(t);
+//     }
+//     else
+//         console.log(grt[0]);
+
+// }
+
+// let a=[10,5,11,20,12],greater_num=[];
+// for(i=0;i<a.length;i++){
+//     for(j=0;j<a.length;j++){
+//         if(a[i]<a[j]){
+//             greater_num.push(a[j]);
+//         }
+        
+//     }
+//     console.log(greater_num);
+//     small_large(greater_num);
+//     great=[];
+// }
+
+// let b=[80,15,75,22,-10,-100,2],arr=[];
+// for(i=0;i<b.length;i++){
+//     for(j=0;j<b.length;j++){
+//         if(b[i]<b[j]){
+//             arr.push(b[j]);
+//         }
+//     }
+//     number(arr);
+//     arr=[];
+// }
+// function number(arr){
+//     arr=arr.sort(function(a,b){return a-b});
+//     if(arr==""){
+//         console.log("-1");
+//     }
+//         else{
+//            console.log(arr[0]); 
+//         }
+//     }
+
+// 1.Write a program to Check whether a date is a weekend date or not?
+// let date=new Date("09/11/2022"),
+// day=date.getDay(date);
+// console.log(day);
+// if(day=="0"||day=="6"){
+// 	console.log("Given Date is weekend");
+// }
+// else{
+// 	console.log("Given Date is not a weekend");
+// }
+
+
+// 2.Write a program to calculate age using JS date
+// let dob = new Date("02/27/2000") ;
+// // console.log(dob);
+// let time=dob.getTime(); 
+// // console.log(time);
+// let total = Date.now() - time;
+// // console.log(total);   
+// let age_cal = new Date(total);
+// // console.log(age_cal);
+// let year = age_cal.getFullYear();
+// // console.log(year);
+// let age = Math.abs(year - 1970); 
+// console.log("Age :"+age); 
+
+
+// let d=17,mon=09,yr=2022;
+// date=mon+"/"+d+"/"+yr;
+// console.log(date);
+// //let gn_date=new Date("09/17/2022"),
+// let gn_date=new Date(date);
+// console.log(gn_date);
+// day=gn_date.getDay(gn_date);
+// out=gn_date-((24*60*60*1000)*(day-1));
+// output=new Date.getTime(out);
+// console.log(getTime(out))
+
+// Generate/Print nearest largest number
+// function small(number){
+//     number=number.sort(function(a,b){return a-b})
+//     if(number==""){
+//     console.log("-1");
+//     }
+//     else
+//         console.log(number[0]);
+
+// }
+
+// let a=[12,50,80,100,-15,8],number=[];
+// for(i=0;i<a.length;i++){
+//     for(j=0;j<a.length;j++){
+//         if(a[i]<a[j]){
+//             number.push(a[j]);
+//         }
+        
+//     }
+//     small (number);
+//     number=[];
+// }
+
+
+// // 1. Get time differences in hours between two dates
+// let date1=new Date(2022,09,12,10,00,22);
+// let date2=new Date();
+// // console.log(date2);
+// let old_time=date1.getTime();
+// // console.log(old_time);
+// let crnt_time=date2.getTime();
+// // console.log(crnt_time);
+// let total=crnt_time-old_time;
+// // console.log(total);
+// let hour=Math.floor(total/(60*60)),
+// positive=Math.abs(hour);
+// // console.log(positive);
+// let convert=(positive/(1000));
+// console.log(convert);
+
+
+// 2.  Get the week start date
+// let d=30,mon=07,yr=2002;
+// date=mon+"/"+d+"/"+yr;
+// console.log(date);
+// let gn_date=new Date(date);
+// console.log(gn_date);
+// fn_day=gn_date.getDay(gn_date);
+// console.log(fn_day);
+// out=gn_date-((24*60*60*1000)*(day-1));
+// console.log(out);
+// output=new Date(out);
+// console.log(output);
+
+
+// Changing Color of <h1> by getting the color from from User
+// document.getElementById("tri").addEventListener("click",generate);
+// function generate(){
+// 	let inpt=document.getElementById("col").value,
+// 	name=document.getElementById("head");
+// 	name.style.color=inpt;
+// }
+
+// Generating Random Color Using rgb by clicking the button
+// function randomcolor(){
+// 	let a=Math.trunc(Math.random()*256),
+// 	b=Math.trunc(Math.random()*256),
+// 	c=Math.trunc(Math.random()*256),colors;
+// 	colors="rgb("+a+","+b+","+c+")";
+// 	document.body.style.background=colors;
+// 	console.log(colors);
+// }
+// let back=document.getElementById("but");
+// back.addEventListener("click",randomcolor);
