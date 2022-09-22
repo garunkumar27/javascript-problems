a4be3ea27840c1110c79ff62dcf17ff578c4f74e
arun kumar <garunk.27@gmail.com>
2022-09-22 08:21:35 +0530

    Practice.

---
+++ x_practice/script.js
@@ -0,0 +1,60 @@
+// fn_day-1 is given to subtract the date (i.e 6-1=5)
+// We need to get the output only as Monday.
+// let d=30,mon=07,yr=2002;
+// date=mon+"/"+d+"/"+yr;
+// console.log(date);
+// let gn_date=new Date(date);
+// console.log(gn_date);
+// fn_day=gn_date.getDay(gn_date);
+// console.log(fn_day);
+// out=gn_date-((24*60*60*1000)*(fn_day-1));
+// console.log(out);
+// output=new Date(out);
+// console.log(output);
+
+// let str=prompt("Enter the Word"),check;
+// check=str.split("");
+// check=check.reverse();
+// check=check.join("");
+// if(str==check){
+//     console.log("Given Word is Palindrome");
+// }
+// else{
+//     console.log("Given Word is not a Palindrome");
+// }
+
+// Age Calculate
+// document.getElementById("trigger").addEventListener("click",calculate);
+// function calculate(){
+//     let dob=document.getElementById("dob").value,
+//     dob_age=new Date(dob),
+//     time=dob_age.getTime(),
+//     cal=Date.now()-time,
+//     age_calc=new Date(cal),
+//     year=age_calc.getFullYear(),
+//     age=Math.abs(year-1970);
+//     document.write("Age is ",age);
+//     console.log(age);
+
+// }
+
+// let inpt=prompt("Enter the Value"),
+// number=inpt.replace(/[^a-zA-Z]/g,"");
+// document.write(number);
+// let str=inpt.replace(/[^0-9]/g,"");
+// document.write(str);
+
+// function omit(get){
+//     let num=get.replace(/[^a-zA-Z]/g,""),
+//     alpha=get.replace(/[^0-9]/g,"");
+//     return  num+alpha;
+   
+// }
+// let para=prompt("Enter the value"),
+// res=omit(para);
+// document.write(res);
+
+// let mail="arun2722000@gmail.com",
+// clear=mail.replace(/[^a-zA-Z]/g,"");
+// document.write(clear);
+
