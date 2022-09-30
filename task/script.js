9457269a9c8f30e6a9b707e32964b59a4f99e3e3
garunkumar27 <garunk.27@gmail.com>
2022-09-30 12:27:42 +0530

    Js task.

--- task/script.js
+++ task/script.js
@@ -229,6 +229,8 @@
 // randomcolor();
 // // setInterval(randomcolor,2000);
 
+
+// Print the number in the reverse as a string
 // let n=parseInt(prompt("enter the number")),remain,a=[],i=0,
 // arr=["zero","one","two","three","four","five","six","seven","eight","nine"];
 // while (n > 0) {
@@ -239,6 +241,7 @@
 // 		console.log(arr[i]);
 // 	}
 
+// Print the nearest larger number one by one
 // let b=[80,15,75,22,-10,-100,2],arr=[];
 // for(i=0;i<b.length;i++){
 //     for(j=0;j<b.length;j++){
@@ -259,7 +262,7 @@
 //         }
 //     }
 
-// 1.Write a program to Check whether a date is a weekend date or not?
+// Write a program to Check whether a date is a weekend date or not?
 // let date=new Date("09/11/2022"),
 // day=date.getDay(date);
 // console.log(day);
@@ -326,7 +329,7 @@
 // console.log(gn_date);
 // fn_day=gn_date.getDay(gn_date);
 // console.log(fn_day);
-// out=gn_date-((24*60*60*1000)*(day-1));
+// out=gn_date-((24*60*60*1000)*(fn_day-1));
 // console.log(out);
 // output=new Date(out);
 // console.log(output);
@@ -350,3 +353,156 @@
 	
 // })
 // console.log(a);
+
+// let str=prompt("Enter the Word"),check;
+// check=str.split("");
+// console.log(check);
+// check=check.reverse();
+// check=check.join("");
+// if(str==check){
+//     console.log("Given Word is Palindrome");
+// }
+// else{
+//     console.log("Given Word is not a Palindrome");
+// }
+
+// Age Calculate
+// document.getElementById("trigger").addEventListener("click",calculate);
+// function calculate(){
+//     let dob=document.getElementById("dob").value,
+//     dob_age=new Date(dob),
+//     time=dob_age.getTime(),
+//     cal=Date.now()-time,
+//     age_calc=new Date(cal),
+//     year=age_calc.getFullYear(),
+//     age=Math.abs(year-1970);
+//     document.write("Age is ",age);
+//     console.log(age);
+
+// }
+
+// let inpt=prompt("Enter the Value"),
+// number=inpt.replace(/[^a-zA-Z]/g,"");
+// document.write(number);
+// let str=inpt.replace(/[^0-9]/g,"");
+// document.write(str);
+
+// function omit(get){
+//     let num=get.replace(/[^a-zA-Z]/g,""),
+//     alpha=get.replace(/[^0-9]/g,"");
+//     return  num+alpha;
+
+// }
+// let para=prompt("Enter the value"),
+// res=omit(para);
+// document.write(res);
+
+// let mail="arun2722000@gmail.com",
+// clear=mail.replace(/[^a-zA-Z]/g,"");
+// document.write(clear);
+
+// Seperate odd and even numbers
+
+/* document.getElementById("check").addEventListener("click", even);
+function even() {
+    let numb = document.getElementById("numb").value,
+        odd = numb.replace(/[^13579]$/g, "");
+    if (numb == odd) {
+        document.getElementById("result").innerHTML = "The number is Odd";
+    }
+    else {
+        document.getElementById("result").innerHTML = "The Number is Even";
+    }
+} */
+
+// Separating Odd and Even Numbers and Adding them together
+// let arr="235781",sum=0,res,sums=0,result,
+// od=arr.replace(/[^13579]/g,"");
+// document.write(od);
+// console.log(od);
+// let eve=arr.replace(/[^24680]/g,"");
+// console.log(eve);
+// document.write(eve);
+// let spl=od.split("");
+// for(i=0;i<spl.length;i++){
+	// spl[i]=parseInt(spl[i]);
+	// res=sum+spl[i];
+	// sum=res;
+// }
+// document.write(sum);
+// console.log(sum);
+// let sp=eve.split("");
+// for(j=0;j<sp.length;j++){
+	// sp[j]=parseInt(sp[j]);
+	// result=sums+sp[j];
+	// sums=result;
+// }
+// document.write(sums);
+// console.log(sums);
+
+//Write a javascript to get humanized number with the correct suffix such as 1st,2nd,3rd 4th:
+// let number=parseInt(prompt("Enter the Value")),
+// remain1=number%10,
+// remain2=number%100;
+// function nremain(){
+// 	if(remain1==1&&remain2!==11){
+// 		document.write(number+"st");
+// 	}
+// 	else if(remain1==2&&remain2!==12){
+// 		document.write(number+"nd");
+// 	}
+// 	else if(remain1==3&&remain2!==13){
+// 		document.write(number+"rd")
+// 	}
+// 	else{
+// 		document.write(number+"th")
+// 	}
+// }
+// nremain();
+
+/* let rang=200,arr=[],sep,val,val2;
+for(i=1;i<rang;i++){
+	if(i%7==0){
+		sep=i;
+		// console.log(sep);
+		arr.push(sep);
+	}
+}
+// console.log(arr);
+for(i=0;i<arr.length;i++){
+	val=arr[i];
+	val=String(val);
+	val=val.split("");
+	// console.log(val);
+	val2=val;
+}
+	for(j=0;j<val2.length;j++){
+		if(val2[0]%10<val2[1]%10||val2[0]%10<val2[1]%10<val2[2]%10){
+			// console.log(val2[0]);
+			val2=val.join("");
+			console.log(val2);
+		}
+	}
+
+ */
+ 
+	 const names=[
+	 {
+		 Name:"Arun",
+		 Ed:"B Tech",
+		 Place:"Sivasailam"
+	 },{
+		 Name:"Kumar",
+		 Ed:"B Tech",
+		 Place:"Sivasailam"
+	 }
+	 ];
+	 let count=0;
+	 
+	names.forEach((key)=>{
+		for(x in key){
+			count++;
+		
+		}
+ });
+ console.log(count);
