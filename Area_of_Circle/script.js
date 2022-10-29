9302fc320fd820e67adb97197c4f40ff5fa77cba
garunkumar27 <garunk.27@gmail.com>
2022-10-29 22:43:21 +0530

    Circle

--- Area_of_Circle/script.js
+++ Area_of_Circle/script.js
@@ -1,6 +1,6 @@
 // Area of Circle
 let A,c,r;// variable declaration
-c=3.14;// variable definition
+c=3.14;// variable definition (c=pi)
 r=parseInt(prompt('Give radius'))//To get Input from user
 A=c*r**2;// expression
 console.log(A);// printing statement
