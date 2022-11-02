c853c961aa403624dba847ce404186eaf163d224
garunkumar27 <garunk.27@gmail.com>
2022-11-02 14:32:40 +0530

    Task

--- task/script2.js
+++ task/script2.js
@@ -38,21 +38,110 @@ else
 arr=arr.join("");
 console.log(arr); */
 
-//Write a program to find duplicate values in a  array
-let array=[1,2,4,5,2,4,1,9,8,6,9],count=0,ab;
+//Write a program to find duplicate values in a array
+/* let array=[1,2,4,5,2,4,1,9,8,6,9],count=0,ab;
 for(i=0;i<array.length;i++){
 	for(j=i+1;j<array.length;j++){
 		if(array[i]==array[j]){
-			count++;
-			console.log(count);
-			//console.log(array[i],"," ,count,"times");
-			
-			
+	
+			console.log(array[i]);
 		}
 
 	}
 	
 }  
-//console.log(count);
-		
+ */
+//Duplicates value of array and also how many times it get repeated
+/* let values=[1,1,2,5,2,4,5,6,8,7,8,9,1],object={};
+values.forEach((numb)=>{
+	object[numb]=(object[numb]||0)+1;
+})
+console.log(object);
+ */
+//Using Star pattern print Hallow Square
+/* let val="",num=5;
+for(i=0;i<num;i++){
+	for(j=0;j<num;j++){
+		if(i==0||i==num-1){
+			val+="*";
+		}
+			else{
+				if(j==0||j==num-1){
+					val+="*";
+					console.log(val)
+				}
+				else{
+					val+=" ";
+					console.log(val)
+				}
+					
+			}
+			
+	}
+	val+="\n";
+}
+console.log(val)
 
+let number=parseInt(prompt("Enter the Value")),str="";
+for(i=0;i<number;i++){
+	for(j=0;j<number;j++){
+		str+=" *";
+	}
+	str+="<br>"
+} */
+//console.log(str);
+//document.write(str);
+
+//Print Square using Star Pattern
+/* let value="",inte=5;
+for(i=0;i<inte;i++){
+	for(j=0;j<inte;j++){
+		if(i==0||i==inte-1){
+		value+=" *"
+	}
+	else{
+		if(j==0||j==inte-1){
+			value+=" *   ";
+			console.log(value);
+		}
+	else{
+		value+=" "
+		console.log(value);
+	}
+	}
+	
+	}
+	value+="\n";
+}
+console.log(value); */
+
+//Write a program to Print Left bottom triangle of * symbol
+/* let numbs=5,st="";
+for(i=0;i<numbs;i++){
+	for(j=0;j<i;j++){
+		st+="*";
+	}
+	st+="\n";
+}
+console.log(st); */
+
+//Write a program to print Hollow Triangle Star Pattern
+let numb=6;hal="";
+for(i=1;i<=numb;i++){
+	for(j=0;j<i;j++){
+		if(i==numb){
+			hal+="*";
+		}
+		else{
+			if(j==0||j==i-1){
+				hal+="*";
+			}	
+			else{
+				hal+=" ";
+				}
+	}
+	}
+	hal+="\n";
+	
+}
+console.log(hal);
